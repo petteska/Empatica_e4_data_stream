a2062f9aff30916e688345f4461e86aa0af8fa1b
Petter Skau-Nilsen <petteska@stud.ntnu.no>
2021-09-28 17:58:49 +0200

    Streaming data from the Empatica E4

---
+++ kok.py
@@ -0,0 +1,40 @@
+import socket
+import time
+# import pylsl
+
+# SELECT DATA TO STREAM
+acc = True      # 3-axis acceleration
+bvp = True      # Blood Volume Pulse
+gsr = True      # Galvanic Skin Response (Electrodermal Activity)
+tmp = True      # Temperature
+
+serverAddress = '127.0.0.1'
+serverPort = 28000
+bufferSize = 4096
+
+deviceID = 'A02DE7'
+
+def connect():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+
+    print("Connecting to server")
+    s.connect((serverAddress, serverPort))
+    print("Connected to server\n")
+
+    print("Devices available:")
+    s.send("device_list\r\n".encode())
+    response = s.recv(bufferSize)
+    print(response.decode("utf-8"))
+
+    print("Connecting to device")
+    s.send(("device_connect " + deviceID + "\r\n").encode())
+    response = s.recv(bufferSize)
+    print(response.decode("utf-8"))
+
+    print("Pausing data receiving")
+    s.send("pause ON\r\n".encode())
+    response = s.recv(bufferSize)
+    print(response.decode("utf-8"))
+connect()
